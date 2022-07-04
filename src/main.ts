b85c99251bce2cd8027e2bb33128709277745c58
realzzz <real_z@yeah.net>
2022-07-05 01:13:48 +0800

    chore: add pinia-plugin-persistedstate

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,14 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router';
 import { createPinia } from 'pinia'
+import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
 
 const app = createApp(App)
-app.use(createPinia())
+
+const pinia = createPinia()
+pinia.use(piniaPluginPersistedstate)
+app.use(pinia)
+
 app.use(router)
 
 app.mount('#app')
