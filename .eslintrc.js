15270e3f5770abd4af943622794cd35001263c88
realzzz <real_z@yeah.net>
2022-07-06 00:33:43 +0800

    chore: update proj config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,40 +1,16 @@
 module.exports = {
   extends: ['plugin:vue/base'],
   rules: {
-    'vue/script-setup-uses-vars': 'error',
+    'vue/script-setup-uses-vars': 'error'
   },
   env: {
     node: true,
     // The Follow config only works with eslint-plugin-vue v8.0.0+
-    "vue/setup-compiler-macros": true,
+    'vue/setup-compiler-macros': true
+  },
+  parserOptions: {
+    ecmaVersion: 2020, // Allows for the parsing of modern ECMAScript features
+    sourceType: 'module', // Allows for the use of imports
+    parser: '@typescript-eslint/parser' // Specifies the ESLint parser
   }
 }
-
-// module.exports = {
-//   root: true,
-//   env: {
-//     node: true
-//   },
-//   parser: 'vue-eslint-parser',
-//   parserOptions: {
-//     parser: '@typescript-eslint/parser', // Specifies the ESLint parser
-//     ecmaVersion: 2020, // Allows for the parsing of modern ECMAScript features
-//     sourceType: 'module', // Allows for the use of imports
-//     ecmaFeatures: {
-//       jsx: true
-//     }
-//   },
-//   extends: [
-//     'plugin:vue/vue3-recommended',
-//     'plugin:@typescript-eslint/recommended',
-//     'prettier',
-//     'plugin:prettier/recommended'
-//   ],
-//   rules: {
-//     '@typescript-eslint/no-var-requires': 0,
-//     '@typescript-eslint/explicit-module-boundary-types': 'off',
-//     'space-before-function-paren': 0,
-//     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-//     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
-//   }
-// }
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,6 +1,7 @@
 import { defineStore } from 'pinia'
 
 export const userStore = defineStore('user', {
+  persist: true,
   state: () => {
     return { name: 'Maria' }
   },
